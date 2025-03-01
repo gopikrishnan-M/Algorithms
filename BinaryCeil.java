ad3d321fa0707045b7f47debf1475b689ebcac22
gopikrishnan-M <gopikris0021@gmail.com>
2025-03-01 19:50:10 +0530

    checking changes

--- BinaryCeil.java
+++ BinaryCeil.java
@@ -5,11 +5,13 @@ public class BinaryCeil {
         int[]arr={0,1,2,3,4,5,6,7,7,7,7,8,8,9,9,9};
         int target=7;
         System.out.println(binaryCeil(arr,target));
+        System.out.println("we made it");
     }
 
     private static int binaryCeil(int[] arr, int target) {
        /* return the index of the element which is the smallest number greater
          than or equal to the target element*/
+        // finally
         int low=0;
         int high=arr.length-1;
         /*we want to find greater element but if the target is the greatest one then return -1*/
